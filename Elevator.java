2777a436d92d4e931923aa2b89d1b30da08f34b3
patlipinski <Guest@stk-cs01-ws16.westlothian.sch.uk>
2016-09-05 10:45:47 +0100

    hehe

--- Elevator.java
+++ Elevator.java
@@ -1,11 +1,12 @@
 
-
 public class Elevator
 {
     // instance variables - replace the example below with your own
     private int current_floor;
     private int top_floor;
     private int lowest_floor;
+    private int occupants;
+    private int max_occupants;
 
     public Elevator()
     {
@@ -13,30 +14,40 @@ public class Elevator
         current_floor = 0;
         top_floor = 11;
     }
-    
+
     public void ascend() {
-       //tell the lift to go up
-       //add 1 on to the current_floor
-       //if current floor less than top_floor
-       if(current_floor < top_floor) {
-        current_floor = current_floor +1;
+        //tell the lift to go up
+        //add 1 on to the current_floor
+        //if current floor less than top_floor
+        if(current_floor < top_floor) {
+            current_floor = current_floor +1;
         }
         else {
             System.out.println("Sorry your at the top!");
         }
-       //else display error message
-     }
-     
+        //else display error message
+    }
+
     public void descend() {
-       //tell the lift to go up
-       //add 1 on to the current_floor
-       //if current floor less than top_floor
-       if(current_floor > lowest_floor) {
-        current_floor = current_floor -1;
+        //tell the lift to go up
+        //add 1 on to the current_floor
+        //if current floor less than top_floor
+        if(current_floor > lowest_floor) {
+            current_floor = current_floor -1;
         }
         else {
             System.out.println("Sorry your at the bottom!");
         }
-       //else display error message
-     }
+        //else display error message
+    }
+
+    public void board() {
+        if (occupants > max_occupants) {
+            occupants = occupants +1;
+        }
+        else
+        {
+            System.out.println("sorry the elevator is full!");
+        }
+    }
 }
